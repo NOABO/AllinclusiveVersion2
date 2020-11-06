f67223e71592dbd894b8bb0d522944dc9ff8d8d4
BasmaBenNasr <bennasrbasma97@gmail.com>
2020-11-06 18:19:07 +0100

    blog delete

--- angularFrontend/src/app/app-routing.module.ts
+++ angularFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompanyComponent } from './company/company.component';
-import { AboutUsComponent } from './component/about-us/about-us.component';
 import { BlogComponent } from './component/blog/blog.component';
 import { HomeComponent } from './component/home/home.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -17,8 +16,7 @@ const routes: Routes = [
   {path:'event-customer', component:EventCustomerComponent},
   {path:'eventmakeraccount',component:EventmakeraccountComponent},
   {path: 'blog', component:BlogComponent},
-  {path:'home',component:HomeComponent},
-  {path:'aboutUs',component:AboutUsComponent}
+  {path:'home',component:HomeComponent}
 
 ];
 
