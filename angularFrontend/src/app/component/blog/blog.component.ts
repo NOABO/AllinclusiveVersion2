e8e7f5bf23f4d5aea3b5a206cdd83f988b1c0486
BasmaBenNasr <bennasrbasma97@gmail.com>
2020-11-06 20:01:36 +0100

    fixing the functionalities of buttons like and dislike

--- angularFrontend/src/app/component/blog/blog.component.ts
+++ angularFrontend/src/app/component/blog/blog.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -14,59 +13,19 @@ customers : any=[{name:'bassoum',message:'my team im really proud of you ❤️'
  times : any= moment().format('MMMM Do YYYY, h:mm:ss a');
  time : any= [];
  postComment = [];
- rating = 0;
- starCount = 5;
- ratingArr: boolean[] = []; // true = solid star; false = empty star
-
- snackBarDuration = 1000;
- response = [
-   'You broke my heart!',
-   'Really?',
-   'We will do better next time.',
-   'Glad you like it!',
-   'Thank you so much!'
- ];
+ post(){
+   console.log(this.time)
+   this.postComment.push(this.comment);
+   this.comment = "";
+   this.time.push(this.times);
+ }
  
- snackBar
-constructor(  snackBar: MatSnackBar) { 
-   // default to no rating, i.e. all empty stars
-   this.ratingArr = Array(this.starCount).fill(false);
-}
-post(){
-  console.log(this.time)
-  this.postComment.push(this.comment);
-  this.comment = "";
-  this.time.push(this.times);
-}
-numberOfLikes : number = 0;
+constructor() { 
   
-likeButtonClick() {
-  this.numberOfLikes++;
 }
 
-dislikeButtonClick() {
-  this.numberOfLikes--;
-}
   ngOnInit(): void {
    
   }
 
-  returnStar(i: number) {
-    if (this.rating >= i + 1) {
-      return 'star';
-    } else {
-      return 'star_border';
-    }
-  }
-
-  onClick(i: number) {
-    this.rating = i + 1;
-    this.snackBar.open(this.response[i], '', {
-      duration: this.snackBarDuration,
-      panelClass: ['snack-bar']
-    });
-
-  }
-
 }
-6
