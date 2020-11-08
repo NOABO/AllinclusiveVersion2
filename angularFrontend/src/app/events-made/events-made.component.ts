824022d4d9dc4afa54b83b949405bcea857489fc
unknown <smaouiali22@gmail.com>
2020-11-08 14:20:18 +0100

    working on filtring events by id

--- angularFrontend/src/app/events-made/events-made.component.ts
+++ angularFrontend/src/app/events-made/events-made.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
+// import { DomSanitizer,SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-events-made',
   templateUrl: './events-made.component.html',
@@ -8,16 +9,28 @@ import { HttpService } from '../http.service';
 export class EventsMadeComponent implements OnInit {
   
 data;
-  constructor(private _http: HttpService) {}
+// untrustedElocURL:string;
+// untrustedEvidURL:string;
+// trustedEvidURL:SafeUrl
+// trustedElocURL:SafeUrl
+  constructor(private _http: HttpService) {
+   
+      // this.untrustedElocURL='https://www.youtube.com';
+      // this.untrustedEvidURL='https://www.google.com/maps/';
+      // this.trustedElocURL=this.sanitizer.bypassSecurityTrustResourceUrl(this.untrustedElocURL);
+      // this.trustedEvidURL=this.sanitizer.bypassSecurityTrustResourceUrl(this.untrustedEvidURL);
+    
+    
+  }
 
   ngOnInit(): void {
     this._http.getEvents().subscribe((data:any[])=>{
       this.data = data
-      console.log(this.data,)
+      console.log(this.data,'getting the events posted')
     })
   }
   showEvents() {
-    this._http
+   
       
   }
 }
